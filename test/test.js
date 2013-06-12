16127e9b5d23b4854d846fd6b9f4a2e341acff7e
bea <bea@ubuntu.(none)>
2013-06-12 13:45:51 +0200

    version 0.0.1

---
+++ test/test.js
@@ -0,0 +1,13 @@
+var GearmanStatus       = require ('../lib/GearmanStatus');
+var port                = 4730;
+var host                = "127.0.0.1";
+var buffer_size         = 10;
+var interval_polling    = 100; //ms
+
+GearmanStatus= new GearmanStatus(port, host, buffer_size, interval_polling);
+
+GearmanStatus.initHistory();            //init the events
+
+
+setInterval (function() { console.log("status", GearmanStatus.writeHistory()); }, 5000);
+
